f5af5ea072b7634ffba1a0ef4e6e56afd4af0ee5
Ferla04 <fer.velas135@gmail.com>
2023-06-07 03:44:48 -0500

    ✅ Feed created

---
+++ components/Feed.jsx
@@ -0,0 +1,53 @@
+'use client'
+import { useEffect, useState } from 'react'
+import { PromptCard } from './PromptCard'
+
+function PromptCardList ({ data, handleTagClick }) {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map(post => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
+
+export function Feed () {
+  const [searchText, setSearchText] = useState('')
+  const [post, setPost] = useState([])
+  const handleSearchChange = event => {}
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch('/api/prompt')
+      const data = await response.json()
+      setPost(data)
+    }
+
+    fetchPost()
+  }, [])
+
+  return (
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+
+      <PromptCardList
+        data={post}
+        handleTagClick={() => {}}
+      />
+    </section>
+  )
+}
