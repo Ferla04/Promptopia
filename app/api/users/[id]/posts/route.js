e24ab905a3a6679885bd34e40fef0fbc47f9c508
Ferla04 <fer.velas135@gmail.com>
2023-06-07 03:39:40 -0500

    ✅ GET user prompts

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,13 @@
+import { connectToDB } from '@utils/database'
+import { PromptModel } from '@models'
+
+export const GET = async (request, { params }) => {
+  try {
+    await connectToDB()
+
+    const prompts = await PromptModel.find({ creator: params.id }).populate('creator')
+    return new Response(JSON.stringify(prompts), { status: 200 })
+  } catch (error) {
+    return new Response('Failed to fecth all prompts', { status: 500 })
+  }
+}
